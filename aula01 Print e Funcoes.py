f596ce84a0902078533e3d7e938b408a8f9213c3
IgordeMoura <igormoura14@yahoo.com.br>
2024-03-05 22:09:58 -0300

    Anotações Referente ao Conteudo Sobre Concatenações em Print e Funções Matematicas e Randomicas

---
+++ aula01 Print e Funcoes.py
@@ -0,0 +1,54 @@
+# curso = "Python"
+# ano = 2024
+# valor = 1500.00
+
+# Concatenação com Vírgula
+# print("O Curso de ", curso, " possui o valor de ", valor, " no ano de ", ano)
+
+
+
+# Usando Identificadores
+# %s = string
+# %f = ponto flutuante
+# %d = inteiro
+# print("O curso de %s possui o valor de %.2f, no ano de %d" % (curso, valor, ano))
+
+
+
+# Operador + -> Juntar Textos
+# print("O curso de " + curso + " possui o valor de " + str(valor) + ", no ano de " + str(ano))
+
+
+
+# armazenando valores na variavel e depois apresentando ela na tela
+# mensagem = "O curso de " + curso + " possui o valor de " + str(valor) + ", no ano de " + str(ano)
+# print(mensagem)
+
+#_________________________________________________________________________________________________
+
+#                     Funções Matematicas
+
+# import math
+
+# print('Raiz Quadrada de 49: ', math.sqrt(49))
+# print('Cosseno de 180 graus: ', math.cos(180))
+# print('Seno de 360 graus: ', math.sin(360))
+# print('Tangente de 180 graus: ', math.tan(180))
+# print('Angulo de 90 graus: ', math.radians(90))
+# print('PI: ', math.pi)
+# print('Hipotenusa de 4 e 8: ', math.hypot(4, 8))
+# print('Arredonda o valor para cima 5.854: ', math.ceil(5.854))
+
+#_________________________________________________________________________________________________
+
+#                     Funções Randomicas
+
+# import random
+
+# print('random.randrange(100): ', random.randrange(100))
+# print('random.randrange(50, 100): ', random.randrange(50, 100))
+# print('random.choice([20, 5, 10, 15, 54]): ', random.choice([20, 5, 10, 15, 54]))
+# print('random.random(): ', random.random())
+# print('random.randint (30, 60): ', random.randint (30, 60))
+
+
