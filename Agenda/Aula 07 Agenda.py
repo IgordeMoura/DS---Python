a4ad09bc646d07d626a8833046b4cd19f8f885a1
IgordeMoura <igormoura14@yahoo.com.br>
2024-05-14 22:24:32 -0300

    Aula 09 dia 14/05 - Igor

--- Agenda/Aula 07 Agenda.py
+++ Agenda/Aula 07 Agenda.py
@@ -2,7 +2,6 @@ from PyQt5 import uic, QtWidgets
 import mysql.connector
 from reportlab.pdfgen import canvas
 
-
 banco = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -17,8 +16,10 @@ def cadastrarContato():
     
     if agenda.rbResidencial.isChecked():
         tipoTelefone="Residencial"
+        
     elif agenda.rbCelular.isChecked():
         tipoTelefone="Celular"
+        
     else:
         tipoTelefone="Não Informado"
         
@@ -28,9 +29,29 @@ def cadastrarContato():
     cursor.execute(comando_SQL,dados)
     banco.commit()
 
+def consultarContatos():
+    listarContatos.show()
+    
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM contatos"
+    cursor.execute(comando_SQL)
+    contatosLidos = cursor.fetchall()
+    
+    listarContatos.tabelaContatos.setRowCount(len(contatosLidos))
+    listarContatos.tabelaContatos.setColumnCount(5)
+    
+    for i in range(0, len(contatosLidos)):
+        for f in range(0, 5):
+            listarContatos.tabelaContatos.setItem(i, f, QtWidgets.QTableWidgetItem(str(contatosLidos[i][f])))
+
 app = QtWidgets.QApplication([])
 agenda=uic.loadUi('Aula 07 Agenda.ui')
+listarContatos = uic.loadUi('Aula 08 Contatos.ui')
+
 agenda.btnCadastro.clicked.connect(cadastrarContato)
+agenda.btnConsultar.clicked.connect(consultarContatos)
+
+# listarContatos.btnGerarPDF.clicked.connect('Gerar PDF')
 
 agenda.show()
 app.exec()
