f29eea9b338f448dbf29548306a38622e30ebe87
IgordeMoura <igormoura14@yahoo.com.br>
2024-05-21 22:31:58 -0300

    Gerando PDF - Igor

--- Agenda/Aula 07 Agenda.py
+++ Agenda/Aula 07 Agenda.py
@@ -44,6 +44,47 @@ def consultarContatos():
         for f in range(0, 5):
             listarContatos.tabelaContatos.setItem(i, f, QtWidgets.QTableWidgetItem(str(contatosLidos[i][f])))
 
+def gerarPDF():
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM contatos"
+    cursor.execute(comando_SQL)
+    contatos_lidos = cursor.fetchall()
+    
+    y = 0 
+    pdf = canvas.Canvas("lista_contatos.pdf")
+    pdf.setFont("Times-Bold", 25)
+    pdf.drawString(200, 800, "Lista de Contatos")
+    
+    pdf.setFont("Times-Bold", 10)
+    pdf.drawString(10, 750, "ID")
+    pdf.drawString(110, 750, "NOME")
+    pdf.drawString(210, 750, "EMAIL")
+    pdf.drawString(370, 750, "TELEFONE")
+    pdf.drawString(470, 750, "TIPO DE CONTATO")
+    
+    for i in range(0, len (contatos_lidos)):
+        y = y + 50
+        pdf.drawString(10, 750 - y, str(contatos_lidos[i][0]))
+        pdf.drawString(110, 750 - y, str(contatos_lidos[i][1]))
+        pdf.drawString(210, 750 - y, str(contatos_lidos[i][2]))
+        pdf.drawString(370, 750 - y, str(contatos_lidos[i][3]))
+        pdf.drawString(470, 750 - y, str(contatos_lidos[i][4]))
+        
+    pdf.save()
+    print("PDF gerado com sucesso!")
+
+def excluirContato():
+    linhaContato = listarContatos.tabelaContatos.currentRow()
+    listarContatos.tabelaContatos.removeRow(linhaContato)
+    
+    cursor = banco.cursor()
+    comando_SQL = "SELECT id FROM contatos"
+    cursor.execute(comando_SQL)
+    contatos_lidos = cursor.fetchall()
+    valorId = contatos_lidos[linhaContato][0]
+    cursor.execute("DELETE FROM contatos WHERE id=" + str(valorId))
+    banco.commit()
+
 app = QtWidgets.QApplication([])
 agenda=uic.loadUi('Aula 07 Agenda.ui')
 listarContatos = uic.loadUi('Aula 08 Contatos.ui')
@@ -51,7 +92,8 @@ listarContatos = uic.loadUi('Aula 08 Contatos.ui')
 agenda.btnCadastro.clicked.connect(cadastrarContato)
 agenda.btnConsultar.clicked.connect(consultarContatos)
 
-# listarContatos.btnGerarPDF.clicked.connect('Gerar PDF')
+listarContatos.btnGerarPDF.clicked.connect(gerarPDF)
+listarContatos.btnDeleteContato.clicked.connect(excluirContato)
 
 agenda.show()
 app.exec()
